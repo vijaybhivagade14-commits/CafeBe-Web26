7fa8bcb96d7dd6186c7319486920c4f77a3a5753
Vijay_dev_2026 <vijaybhivagade14@gmail.com>
2026-07-18 17:33:27 +0530

    Updated MenuHero routs and Commnd SpecialMenu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import MenuSection from '../components/Menu';
 import SpecialMenu from '../components/SpecialMenu';
 import Footer from '../components/Footer';
+import MenuHero from '../components/MenuHero/MenuHero';
 
 export default function MenuPage() {
   return (
     <>
-      <SpecialMenu />
-      <MenuSection />
+      <MenuHero />
+
+      {/* <SpecialMenu /> */}
+      <MenuSection isDetailedView={true} />
       <Footer />
     </>
   );
